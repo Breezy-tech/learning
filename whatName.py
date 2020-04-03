5cce7e8fad62ad5b409a0eb3b694f32f4ae94073
Ty <mrozektyler@gmail.com>
2020-04-02 21:59:06 -0500

    My First Commit

---
+++ whatName.py
@@ -0,0 +1,10 @@
+print("Hi!")
+name = input("What's your name? : ")
+print(f"Hi {name}!")
+shower = input(f"How was your shower {name}? (good or bad): ").lower()
+if shower == "good":
+	print(f"I'm glad to hear that your shower was {shower}!")
+elif shower == "bad":
+	print(f"I'm sorry to hear that you shower was {shower}.")
+else:
+	print(f"Next time please pick good or bad!")
